7e78edf4b410414ca778b71aa71d611a07a1acaf
rey-vthi <108cs16047@gmail.com>
2020-12-22 20:11:34 +0530

    added test for notifying manager and attendant

--- test/patterns/ParkingLotTest.java
+++ test/patterns/ParkingLotTest.java
@@ -9,28 +9,54 @@ import static org.mockito.Mockito.*;
 
 public class ParkingLotTest {
     @Test
-    public void shouldParkWhenSlotsAreAvailable()  {
+    public void shouldParkWhenSlotsAreAvailable() {
         ParkingLot parkingLot = new ParkingLot(1);
         assertTrue(parkingLot.park());
     }
 
     @Test
-    public void shouldNotParkWhenSlotsAreNotAvailable()  {
+    public void shouldNotParkWhenSlotsAreNotAvailable() {
         ParkingLot parkingLot = new ParkingLot(1);
         parkingLot.park();
         assertFalse(parkingLot.park());
     }
 
     @Test
-    public void shouldNotifyAssistantWhenParkingLotIsFull()  {
+    public void shouldNotifyAssistantWhenParkingLotIsFull() {
         Assistant assistant = mock(Assistant.class);
         ParkingLot parkingLot = new ParkingLot(1);
         HashSet<Double> occupancy = new HashSet<>();
         occupancy.add(100.0);
         parkingLot.addSpectator(assistant::notify, occupancy);
-        assertTrue(parkingLot.park());
+        parkingLot.park();
 
         verify(assistant).notify(parkingLot, 100.0);
     }
 
+    @Test
+    public void shouldNotifyAttendantWhenParkingLotIsFull() {
+        ParkingLotSpectator attendant = mock(ParkingLotSpectator.class);
+        ParkingLot parkingLot = new ParkingLot(1);
+        HashSet<Double> occupancy = new HashSet<>();
+        occupancy.add(100.0);
+        parkingLot.addSpectator(attendant::notify, occupancy);
+        parkingLot.park();
+
+        verify(attendant).notify(parkingLot, 100.0);
+    }
+
+    @Test
+    public void shouldNotifyManagerWhenParkingLotIs80Percent() {
+        ParkingLotSpectator manager = mock(ParkingLotSpectator.class);
+        ParkingLot parkingLot = new ParkingLot(5);
+        HashSet<Double> occupancy = new HashSet<>();
+        occupancy.add(80.0);
+        parkingLot.addSpectator(manager::notify, occupancy);
+        parkingLot.park();
+        parkingLot.park();
+        parkingLot.park();
+        parkingLot.park();
+
+        verify(manager).notify(parkingLot, 80.0);
+    }
 }
